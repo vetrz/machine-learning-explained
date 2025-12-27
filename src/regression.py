348da82403e3f876bb3ba49139b5abeee894b604
Vitor Cardoso De Moraes <moraesvitor29@gmail.com>
2025-12-26 21:57:15 -0300

    fix: ajuste no modelo de regressão linear

--- src/regression.py
+++ src/regression.py
@@ -1,46 +1,37 @@
 import numpy as np
 
-
 class LinearRegression_:
     def __init__(self, random_state=42):
         self._coef = None
         self._intercept = None
-
         self.random_state = random_state
         self._rng = np.random.default_rng(self.random_state)
+        self.cost_history = []
 
     def _hypothesis(self, X, theta):
         return np.dot(X, theta)
-
-    def _cost(self, X, y, theta):
-        m = y.shape[0]
-        predictions = self._hypothesis(X, theta)
-        sq_errors = (predictions - y) ** 2
-        J = 1 / 2 * (1 / m) * np.sum(sq_errors)
-
-        return J
-
-    def _gradient_descent(self, X, y, theta, alpha):
+  
+    def _compute_step(self, X, y, theta, alpha):
         m = y.shape[0]
         predictions = self._hypothesis(X, theta)
         errors = predictions - y
 
-        theta = theta - (alpha / m) * (X.T.dot(errors))
+        gradient = (1/m) * X.T.dot(errors)
+        cost = (1 / (2 * m)) * np.sum(errors**2)
 
-        J = 1 / 2 * (1 / m) * np.sum(errors**2)
+        theta = theta - alpha * gradient
 
-        return theta, J
+        return theta, cost
 
-    def fit(self, X, y, num_iters=600, alpha=0.001):
-        self.cost_history = []
+    def fit(self, X, y, num_iters=800, alpha=0.05):
         m, n = X.shape
-        theta = self._rng.random(n + 1)
+        theta = self._rng.standard_normal(n+1) * np.sqrt(1/n)
 
-        X = np.c_[np.ones(m), X]
-        y = y.ravel()
+        X_b = np.c_[np.ones(m), X]
+        y_b = y.ravel()
 
         for _ in range(num_iters):
-            theta, cost = self._gradient_descent(X, y, theta, alpha)
+            theta, cost = self._compute_step(X_b, y_b, theta, alpha)
             self.cost_history.append(cost)
 
         self._intercept = theta[0]
